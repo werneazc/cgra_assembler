0f13624d19d4576f2814338bf8c9154edfb35d36
andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
2019-06-02 12:36:48 +0200

    Add baseclass for levels in assembler files
    Levels are created by starting a new scope for instance for loops or
    conditions. A level is created by entering a new scope and is deleted
    by leaving the scope.
    
    Signed-off-by: andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>

---
+++ header/level.h
@@ -0,0 +1,175 @@
+/*
+ * Copyright (C) 2019  andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LEVEL_H
+#define LEVEL_H
+
+#include <cstdint>
+#include <vector>
+#include "parseobjbase.h"
+
+namespace as {
+
+/**
+* @class Level
+* 
+* @brief Level in a VCGRA assembler file
+* 
+* @details
+* A level is used to define visibility of variables and to design control flow.
+* A LOOP for instance opens a new Level and can interact with variables from its
+* parent Level or from itself. A Level includes parse objects and child levels.
+* 
+*/
+class Level
+{
+public:
+    
+    /**
+    * @brief Empty constructor
+    * 
+    */
+    Level();
+    
+    /**
+    * @brief Standard constructor to add a new child level
+    * 
+    * @param[in] parentLvlA parentLvlA: Adds new child level to actual level.
+    */
+    Level(Level* parentLvlA);
+    
+    /**
+    * @brief Move constructor
+    * 
+    * @param[in] src src: Source from where to move content. Members of source going to be deleted.
+    */
+    Level(Level&& src);
+    
+    /**
+    * @brief Move assignment
+    * 
+    * @param[in] src src: Source from where to move content. Members of source going to be deleted.
+    * @return as::Level& New assigned level element.
+    */
+    Level& operator=(Level&& src);
+    
+    /**
+    * @brief Desctrutor
+    * 
+    * @details
+    * Delete function is called on all items in m_parsedObjVec and m_childLvlVec.
+    * 
+    */
+    virtual ~Level();
+    
+    //Actions with parsed objects
+    /**
+    * @brief Add parse object to current level.
+    * 
+    * @param[in] pObjA pObjA: Object pointer of parsed object to add.
+    * @return uint8_t 0=success, 1=failure.    
+    */
+    virtual uint8_t addParseObj(ParseObjBase* pObjA) final;
+    
+    /**
+    * @brief Get parsed object from m_parsedObjVec.
+    * 
+    * @param[in] idxA idxA: Element index of m_parsedObjVec vector.
+    * @return ParseObjBase* nullptr, if element not found or idxA out of range
+    */
+    virtual ParseObjBase* getParseObj(uint32_t idxA) const final;
+    
+    /**
+    * @brief Delete parsed object from m_parsedObjVec.
+    * 
+    * @param[in] idxA idxA: Index of item in m_parsedObjVec going to be deleted.
+    * @return as::ParseObjBase* nullptr if idxA out of range, otherwise pointer to deleted item.
+    */
+    virtual ParseObjBase* deleteParseObj(uint32_t idxA) final;
+    
+    /**
+    * @brief Find parsed object by name in current level and parent level.
+    * 
+    * @param[in] nameA nameA: name of variable to find.
+    * @return as::ParseObjBase* nullptr, if variable is not found by its name, else pointer to parse object.
+    */
+    virtual ParseObjBase* findParseObj(const std::string& nameA) final;
+    
+    /**
+    * @brief Add parse object to current level.
+    * 
+    * @param[in] lvlA lvlA: Level where the parsed object should be added
+    * @param[in] parseObjA parseObjA: Parsed object to add.
+    * @return as::Level& Level where the parsed object currently added.
+    */
+    friend Level& operator<<(Level& lvlA, ParseObjBase* parseObjA);
+    
+    //Level adapting class methods
+    /**
+    * @brief Clear m_parsedObjVec. All items are deleted from head.
+    * 
+    * @return uint8_t 0=success, 1=failure.
+    */
+    virtual uint8_t clearParseObjList() final;
+    
+    /**
+    * @brief Get access to m_parsedObjVec
+    * 
+    * @return const std::vector<ParseObjBase*>& Reference to m_parsedObjVec variable. 
+    */
+    virtual const std::vector<ParseObjBase*>& getParseObjList() const final;
+    
+    /**
+    * @brief Leave current level and set parent level as active level.
+    */
+    virtual void leave(void);
+   
+    //Static class methods:
+    /**
+    * @brief Set current active level activeLvl.
+    * 
+    * @param lvlA lvlA: Pointer to next active level.
+    */
+    static void setCurrentLevel(Level* lvlA);
+    
+    /**
+    * @brief Return static class member activeLvl
+    * 
+    * @return Level*
+    */
+    static Level* getCurrentLevel(void);
+    
+private:
+    //Forbidden constructors
+    Level(const Level& src) = delete;
+    Level& operator=(const Level& src) = delete;
+    
+    //Class Members
+    std::vector<Level*> m_childLvlVec{};
+    //!< @brief Store child levels
+    Level* m_parentLvl;
+    //!< @brief Store parent level
+    std::vector<ParseObjBase*> m_parsedObjVec{};
+    //!< @brief Store parsed objects of actual level.
+    //Class static members
+    static Level* activeLvl;
+    //!< @brief Currently active level for adding new parsed objects.
+};
+
+} /* End namespace as */
+
+#endif // LEVEL_H
---
+++ src/level.cpp
@@ -0,0 +1,194 @@
+/*
+ * Copyright (C) 2019  andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdexcept>
+#include "level.h"
+#include "../header/myException.h"
+#include "../header/parseobjectvariable.h"
+
+as::Level& operator<<(as::Level& lelA, as::ParseObjBase* parseObjA)
+{
+    if(lelA.addParseObj(parseObjA))
+        throw as::AssemblerException("Error while adding parse object to current level.", 4000);
+    
+    return lelA;
+}
+
+namespace as {
+
+
+Level* Level::activeLvl = nullptr;
+
+void Level::setCurrentLevel(Level* lvlA)
+{
+    Level::activeLvl = lvlA;
+    return;
+}
+
+Level* Level::getCurrentLevel()
+{
+    return Level::activeLvl;
+}
+
+
+Level::Level() : m_parentLvl{nullptr}
+{ }
+
+Level::Level(Level* parantLvlA)
+{
+    if(parantLvlA)
+        m_parentLvl = parantLvlA;
+    else
+        throw std::invalid_argument("Parent Level cannot be empty.");
+    
+    return;
+}
+
+Level& Level::operator=(Level&& src)
+{
+    this->m_parentLvl = src.m_parentLvl;
+    src.m_parentLvl = nullptr;
+    this->m_childLvlVec = src.m_childLvlVec;
+    src.m_childLvlVec.clear();
+    this->m_parsedObjVec = src.m_parsedObjVec;
+    src.m_parsedObjVec.clear();
+    
+    return *this;
+}
+
+Level::Level(Level&& src)
+{
+    this->m_parentLvl = src.m_parentLvl;
+    src.m_parentLvl = nullptr;
+    this->m_childLvlVec = src.m_childLvlVec;
+    src.m_childLvlVec.clear();
+    this->m_parsedObjVec = src.m_parsedObjVec;
+    src.m_parsedObjVec.clear();
+    
+    return;
+}
+    
+Level::~Level()
+{
+    for(auto* item : m_childLvlVec)
+        delete item;
+    
+    clearParseObjList(); 
+    
+    return;
+}
+
+uint8_t Level::addParseObj(ParseObjBase* pObjA)
+{
+    if(pObjA)
+    {
+        m_parsedObjVec.push_back(pObjA);
+        return 0;
+    }
+    else
+        return 1;
+}
+
+
+const std::vector<ParseObjBase*>& Level::getParseObjList() const
+{
+    return m_parsedObjVec;
+}
+
+ParseObjBase* Level::getParseObj(uint32_t idxA) const
+{
+    try
+    {
+       return m_parsedObjVec.at(idxA);
+    }
+    catch(std::out_of_range& e)
+    {
+        return nullptr;
+    }
+    catch(std::exception& e)
+    {
+        throw e;
+    }
+}
+
+uint8_t Level::clearParseObjList()
+{
+    try {
+        for(auto* item : m_parsedObjVec)
+            delete item;
+        m_parsedObjVec.clear();
+        return 0;
+    }
+    catch(std::exception& e)
+    {
+        return 1;
+    }
+
+}
+
+ParseObjBase* Level::deleteParseObj(uint32_t idxA)
+{
+    try
+    {
+        ParseObjBase* tPtr;
+        tPtr = m_parsedObjVec.at(idxA);
+        m_parsedObjVec.erase(m_parsedObjVec.begin()+idxA);
+        return tPtr;
+    }
+    catch(std::out_of_range& e)
+    {
+        return nullptr;
+    }
+    catch(std::exception& e)
+    {
+        throw e;
+    }
+}
+
+ParseObjBase* Level::findParseObj(const std::string& nameA)
+{
+    
+    ParseObjBase* t_parseObj=nullptr;
+    
+    //Search for variable in current level
+    for(auto* parseObj : m_parsedObjVec)
+    {
+        if(parseObj->getCommandClass() == COMMANDCLASS::VARIABLE)
+        {
+            if(*(dynamic_cast<ParseObjectVariable*>(parseObj)) == nameA)
+            {
+                t_parseObj=parseObj;
+                break;
+            }
+        }
+        else
+            continue;
+    }
+    
+    //Look for variable in parent level
+    t_parseObj = m_parentLvl->findParseObj(nameA);
+        
+    return t_parseObj;
+}
+
+void Level::leave(void)
+{
+    Level::setCurrentLevel(m_parentLvl);
+    return;
+}
+
+} /* End namespace as */
