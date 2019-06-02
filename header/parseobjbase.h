07ca4d9f343e1df906264f88e6b8074f58512363
andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
2019-06-02 12:32:55 +0200

    Add base class for parsed objects of assembler file
    Signed-off-by: andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>

---
+++ header/parseobjbase.h
@@ -0,0 +1,194 @@
+/*
+ * Copyright (C) 2019  andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PARSEOBJBASE_H
+#define PARSEOBJBASE_H
+
+#include <cstdint>
+#include <string>
+
+
+namespace as {
+
+//Type declarations
+class Level;
+
+/**
+* @enum COMMANDCLASS
+* 
+* @brief Types of assembler commands.
+* 
+*/
+enum class COMMANDCLASS : uint8_t {
+    UNKNOWN,        //!< @brief Unknown assembler command (error)
+    CONSTANT,       //!< @brief Constant value in assembler command
+    VARIABLE,       //!< @brief Variable in assembler command
+    NOOPERANT,      //!< @brief Assembler command with no operator
+    ONEOPERANT,     //!< @brief Assembler command with one operator
+    TWOOPERANT,     //!< @brief Assembler command with two operators
+    THREEOPERANT,   //!< @brief Assembler command with three operators
+    ARITHMETIC      //!< @brief Assembler command with arithmetic operations
+};
+
+/**
+* @class ParseObjBase
+* 
+* @brief Base class for parsed objects of assembler code.
+* 
+* @details
+* This class should be used as base class for all valid types of assembler commands.
+* 
+*/
+class ParseObjBase
+{
+public:
+    /**
+    * @brief Standard Constructor
+    * 
+    * @param[in] levelA levelA: Current level, where the parsed object exists.
+    * @param[in] cmdA cmdA: Command type of parsed assembler line
+    * @param[in] cmdLineA cmdLineA: Copy of assembler line as string
+    * @param[in] lineNumberA lineNumberA: Line number in assembler file where the current line exists.
+    */
+    ParseObjBase(Level* levelA, COMMANDCLASS cmdA, const std::string& cmdLineA, const uint32_t lineNumberA);
+    
+    /**
+    * @brief Copy Constructor
+    * 
+    * @param[in] src src: Source of the copy.
+    */
+    ParseObjBase(const ParseObjBase& src);
+    
+    /**
+    * @brief Move Constructor
+    * 
+    * @param[in] src src: Source of new object. The entries of source object are deleted.
+    */
+    ParseObjBase(ParseObjBase&& src);
+    
+    /**
+    * @brief Copy Assignment
+    * 
+    * @param[in] src src: Source of assignment.
+    * 
+    * @return ParseObjBase& new constructed parsed object with values of source.
+    */
+    ParseObjBase& operator=(const ParseObjBase& src);
+    
+    /**
+    * @brief Move Assignment
+    * 
+    * @param[in] src src: Source of assignment. Values of source are deleted.
+    * 
+    * @return ParseObjBase& new constructed parsed object with values of source.
+    */
+    ParseObjBase& operator=(ParseObjBase&& src);
+    
+    /**
+    * @brief Get string of read assembler file line.
+    * 
+    * @return std::string copy of assembler line.
+    */
+    virtual std::string getReadCmdLine() const final;
+    
+    /**
+    * @brief Get line of assembler file.
+    * 
+    * @return uint32_t Assembler file line
+    */
+    virtual uint32_t getFileLineNumber() const final;
+    
+    /**
+    * @brief Get level of parse object.
+    * 
+    * @return Level* Pointer to level where parse object was found.
+    */
+    virtual Level* getLevel() const final;
+    
+    /**
+    * @brief Return command type of assembler command.
+    * 
+    * @return as::COMMANDCLASS Copy of stored assembler command type
+    */
+    virtual COMMANDCLASS getCommandClass(void) const final;
+    
+    /**
+    * @brief Reset all members of class to default values
+    */
+    virtual void clearMembers(void);
+    
+protected:
+    
+    /**
+    * @brief Set new read command line
+    * 
+    * @param[in] newA New value for read command line.
+    * 
+    * @return Copy of previous value.
+    */
+    virtual std::string setCmdReadLine(const std::string& newA) final;
+    
+    /**
+    * @brief Set new line for parsed command in assembler file.
+    * 
+    * @param[in] newA New value for parsed line
+    * @return Copy of previous value.
+    */
+    virtual u_int32_t setLineNumber(const u_int32_t newA) final;
+    
+    /**
+    * @brief Set new level pointer.
+    * 
+    * @param[in] newA New level to set.
+    * @return Copy of previous level pointer.
+    */
+    virtual Level* const setLevel(const Level* newA) final;
+    
+    /**
+    * @brief Set new command class for parsed object
+    * 
+    * @param[in] newA New command class value
+    
+    * @return Copy of previous command class value
+    */
+    virtual COMMANDCLASS setCommandClass(COMMANDCLASS newA) final;
+    
+public: 
+    /**
+    * @brief Destructor
+    * 
+    */
+    virtual ~ParseObjBase() = default;
+    
+private:
+    //Forbidden Constructors
+    ParseObjBase() = delete;
+    
+    //Class member
+    Level* m_level;
+    //!< @brief Stores the pointer to the level, where the parse object is found.
+    COMMANDCLASS m_class;
+    //!< @brief Defines the type of parsed object
+    std::string m_cmdLine;
+    //!< @brief Stores the textual line from assembler file
+    uint32_t m_lineNumber;
+    //!< @brief Stores the line in the assembler file where the parsed object was found.
+};
+
+} /* End namespace as */
+
+#endif // PARSEOBJBASE_H
---
+++ src/parseobjbase.cpp
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2019  andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *invalid_argument
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "../header/parseobjbase.h"
+#include <stdexcept>
+
+
+namespace as {
+
+ParseObjBase::ParseObjBase(Level* levelA, COMMANDCLASS cmdA, const std::string& cmdLineA, const uint32_t lineNumberA) :
+    m_class{cmdA}, m_cmdLine{cmdLineA}, m_lineNumber{lineNumberA}
+{
+    if(levelA)
+        m_level = levelA;
+    else
+        throw std::invalid_argument{"Pointer to current level is invalid."};
+    
+    return;
+}
+
+
+
+ParseObjBase::ParseObjBase(const as::ParseObjBase& src)
+{
+    this->m_level = src.m_level;
+    this->m_class = src.m_class;
+    this->m_cmdLine = src.m_cmdLine;
+    this->m_lineNumber = src.m_lineNumber;
+    
+    return;
+}
+
+ParseObjBase::ParseObjBase(as::ParseObjBase && src)
+{
+    this->m_level = src.m_level;
+    src.m_level = nullptr;
+    this->m_cmdLine = src.m_cmdLine;
+    src.m_cmdLine.clear();
+    this->m_lineNumber = src.m_lineNumber;
+    src.m_lineNumber = 0;
+    this->m_class = src.m_class;
+    src.m_class = COMMANDCLASS::UNKNOWN;
+    
+    return;
+}
+
+ParseObjBase& ParseObjBase::operator=(const ParseObjBase& src)
+{
+    
+    this->m_level = src.m_level;
+    this->m_class = src.m_class;
+    this->m_cmdLine = src.m_cmdLine;
+    this->m_lineNumber = src.m_lineNumber;
+    
+    return *this;
+}
+
+ParseObjBase& ParseObjBase::operator=(as::ParseObjBase && src)
+{
+    
+    this->m_level = src.m_level;
+    src.m_level = nullptr;
+    this->m_cmdLine = src.m_cmdLine;
+    src.m_cmdLine.clear();
+    this->m_lineNumber = src.m_lineNumber;
+    src.m_lineNumber = 0;
+    this->m_class = src.m_class;
+    src.m_class = COMMANDCLASS::UNKNOWN;
+    
+    return *this;
+}
+
+std::string ParseObjBase::getReadCmdLine() const
+{
+    return m_cmdLine;
+}
+
+uint32_t ParseObjBase::getFileLineNumber() const
+{
+    return m_lineNumber;
+}
+
+Level* ParseObjBase::getLevel() const
+{
+    return m_level;
+}
+
+COMMANDCLASS ParseObjBase::getCommandClass() const
+{
+    return m_class;
+}
+
+void ParseObjBase::clearMembers()
+{
+    m_class = COMMANDCLASS::UNKNOWN;
+    m_cmdLine.clear();
+    m_level = nullptr;
+    m_lineNumber = -1;
+    
+    return;
+    
+}
+
+std::string ParseObjBase::setCmdReadLine(const std::string& newA)
+{
+    auto t_str = m_cmdLine;
+    m_cmdLine = newA;
+    
+    return t_str;
+    
+}
+
+as::COMMANDCLASS ParseObjBase::setCommandClass(as::COMMANDCLASS newA)
+{
+    auto t_class = m_class;
+    m_class = newA;
+    
+    return t_class;
+}
+
+Level* const ParseObjBase::setLevel(const as::Level* newA)
+{
+    auto* t_lvl = m_level;
+    m_level = const_cast<Level*>(newA);
+    
+    return t_lvl;
+}
+
+u_int32_t ParseObjBase::setLineNumber(const u_int32_t newA)
+{
+    auto t_num = m_lineNumber;
+    m_lineNumber = newA;
+    
+    return t_num;
+}
+
+} /* End namespace as */
