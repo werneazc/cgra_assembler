6cb2ef15cb2170ca54e0487afc9941d37bfa9db0
andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
2019-06-13 09:35:02 +0200

    Add parse object for addition of two variables to repository (ADD)
    Signed-off-by: andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>

---
+++ header/add.h
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2019  andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ADD_H
+#define ADD_H
+
+#include "parseobjbase.h"
+#include "iarithmetic.h"
+
+namespace as 
+{
+
+/**
+* @class Add
+* 
+* @brief Add two variables and store result in m_first.
+*
+*/
+class Add : public ParseObjBase, public IArithmetic
+{
+public:
+    /**
+    * @brief General constructor
+    * 
+    * @param lvlA Level pointer where the add command exists in assembler file.
+    * @param cmdLineA Command line in assembler file.
+    * @param lineNumberA Line number in assembler file where the command is placed.
+    * @param firstA Pointer to first variable for addition.
+    * @param secondA Pointer to second variable for addition.
+    */
+    Add(Level* lvlA, const std::string& cmdLineA, const uint32_t lineNumberA,
+        ParseObjBase* const firstA = nullptr, ParseObjBase* const secondA = nullptr);
+    
+    /**
+    * @brief Copy constructor
+    * 
+    * @param srcA Source to initialize members of new object.
+    */
+    Add(const Add& srcA);
+    
+    /**
+    * @brief Move constructor
+    * 
+    * @param srcA Source to initialize members of new object. Members of source are deleted.
+    */
+    Add(Add&& srcA);
+    
+    /**
+    * @brief Copy assignment
+    * 
+    * @param lhsA Source for assignment to new object.
+    * @return Object with values of lhsA.
+    */
+    Add& operator=(const Add& lhsA);
+    
+    /**
+    * @brief Move assignment
+    * 
+    * @param lhsA Source for assignment of member variables. Members of lhs are deleted.
+    * @return Object with values of lhsA.
+    */
+    Add& operator=(Add&& lhsA);
+    
+    /**
+    * @brief Destructor
+    */
+    virtual ~Add() = default;
+    
+    /**
+    * @brief Delete all members of object.
+    */
+    virtual void clearMembers() override;
+    
+    /**
+    * @brief Add two variables and store result m_first.
+    * 
+    * @throws AssemblerException if one of the operands is not a variable object.
+    * 
+    * @return Always true (unused)
+    */
+    virtual bool processOperation() override;
+    
+private:
+    //Forbidden constructor
+    Add() = delete;
+};
+
+} /* End namespace as */
+
+#endif // ADD_H
---
+++ src/add.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2019  andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <utility>
+#include "add.h"
+#include "parseobjectvariable.h"
+#include "myException.h"
+
+namespace as 
+{
+Add::Add(Level* lvlA, const std::string& cmdLineA, const uint32_t lineNumberA,
+    ParseObjBase* const firstA, ParseObjBase* const secondA) : 
+    ParseObjBase{lvlA, COMMANDCLASS::ARITHMETIC, cmdLineA, lineNumberA}, IArithmetic{firstA, secondA}
+{ return; }
+
+Add::Add(const Add& srcA) :
+    ParseObjBase{srcA.getLevel(), srcA.getCommandClass(), srcA.getReadCmdLine(),
+        srcA.getFileLineNumber()}, IArithmetic{srcA.getFirst(), srcA.getSecond()}
+{ return; }
+
+Add::Add(Add&& srcA) :
+    ParseObjBase{srcA.getLevel(), srcA.getCommandClass(), srcA.getReadCmdLine(),
+        srcA.getFileLineNumber()}, IArithmetic{srcA.getFirst(), srcA.getSecond()}
+{ 
+    srcA.clearMembers();
+    return; 
+}
+
+Add& Add::operator=(const Add& lhsA)
+{
+    *(static_cast<ParseObjBase*>(this)) = static_cast<const ParseObjBase&>(lhsA);
+    this->setFirst(lhsA.getFirst());
+    this->setSecond(lhsA.getSecond());
+    
+    return *this;
+}
+
+Add& Add::operator=(Add&& lhsA)
+{
+    *(static_cast<ParseObjBase*>(this)) = std::move(static_cast<ParseObjBase&>(lhsA));
+    this->setFirst(lhsA.getFirst());
+    this->setSecond(lhsA.getSecond());
+    
+    lhsA.clearMembers();
+    
+    return *this;
+}
+
+void Add::clearMembers(void)
+{
+    static_cast<ParseObjBase*>(this)->clearMembers();
+    this->setFirst(nullptr);
+    this->setSecond(nullptr);
+    
+    return;
+}
+
+bool Add::processOperation(void)
+{
+    auto t_first = dynamic_cast<ParseObjectVariable* const>(this->getFirst());
+    auto t_second = dynamic_cast<ParseObjectVariable* const>(this->getSecond());
+    
+    if(!(t_first && t_second))
+        throw AssemblerException("Error Addition: One of the operands is not a variable type", 8525);
+    else
+        t_first->setVariableValue(t_first->getVariableValue() + t_second->getVariableValue());
+    
+    return true;
+}
+
+} /* End namespace as */
