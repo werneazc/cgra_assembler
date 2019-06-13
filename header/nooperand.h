ae880c52f269f3dbb3d508c4433ab282f5911e12
andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
2019-06-13 11:00:39 +0200

    Add parse object for no-operand commands
    Signed-off-by: andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>

---
+++ header/nooperand.h
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2019  andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef NOOPERAND_H
+#define NOOPERAND_H
+
+#include <cstdint>
+#include "parseobjbase.h"
+
+namespace as
+{
+
+
+//Forward declaration
+class Level;
+
+    
+/**
+* @class NoOperand
+* 
+* @brief Parse object for assembler operations with no operand.
+* 
+* @details
+* Operands of this kind generate machine codes for VCGRA. Examples are START,
+* WAIT_RAIDY or FINISH. The IDs for machine code are stored in private Member.
+*/
+class NoOperand : public ParseObjBase
+{
+public:
+    /**
+    * @brief General constructor
+    * 
+    * @param lvlA Level where the parse object exists.
+    * @param cmdLineA Assembler file line with command.
+    * @param lineNumberA Assembler file line number.
+    * @param machineIdA Machine Code ID for decode at VCGRA.
+    */
+    NoOperand(Level* const lvlA, const std::string& cmdLineA, const uint32_t lineNumberA,
+        const uint32_t machineIdA);
+    
+    /**
+    * @brief Copy constructor
+    * 
+    * @param srcA Source for members of new object.
+    */
+    NoOperand(const NoOperand& srcA);
+    
+    /**
+    * @brief Move constructor
+    * 
+    * @param srcA Source for members of new object. Members of source are deleted.
+    */
+    NoOperand(NoOperand&& srcA);
+    
+    /**
+    * @brief Copy assignment
+    * 
+    * @param rhsA Source for members of assigned object.
+    * @return Object with member values of rhsA.
+    */
+    NoOperand& operator=(const NoOperand& rhsA);
+    
+    /**
+    * @brief Move assignment
+    * 
+    * @param rhsA Source for members of new object. Members of source are deleted.
+    * @return Object with member values of rhsA.
+    */
+    NoOperand& operator=(NoOperand&& rhsA);
+    
+    /**
+    * @brief Destructor
+    */
+    virtual ~NoOperand(void) = default;
+    
+    /**
+    * @brief Delete all member variables.
+    */
+    virtual void clearMembers(void) override;
+    
+    /**
+    * @brief Get machine code ID.
+    * 
+    * @return Current machine code ID.
+    */
+    uint32_t getMachineCodeId(void) const;
+    
+    /**
+    * @brief Set new machine code ID.
+    * 
+    * @param machineIdA New machine code ID.
+    * @return Former machine code ID.
+    */
+    uint32_t setMachineCodeId(const uint32_t machineIdA);
+    
+private:
+    //Forbidden constructor
+    NoOperand() = delete;
+    
+    //Private member variables
+    uint32_t m_machineCodeID;
+    //!< @brief Machine code Identification code for command fetching and decode.
+};
+
+} /* End namespace as */
+#endif // NOOPERAND_H
---
+++ src/nooperand.cpp
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2019  andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <utility>
+#include "nooperand.h"
+
+
+as::NoOperand::NoOperand(as::Level *const lvlA, const std::string& cmdLineA,
+                         const uint32_t lineNumberA, const uint32_t machineIdA) :
+    as::ParseObjBase{lvlA, as::COMMANDCLASS::NOOPERANT, cmdLineA, lineNumberA},
+    m_machineCodeID{machineIdA}
+{ return; }
+
+as::NoOperand::NoOperand(const as::NoOperand& srcA) :
+    as::ParseObjBase{srcA.getLevel(), srcA.getCommandClass(), srcA.getReadCmdLine(),
+    srcA.getFileLineNumber()}
+{
+    this->m_machineCodeID = srcA.m_machineCodeID;
+    return;
+}
+
+as::NoOperand::NoOperand(as::NoOperand && srcA) :
+    as::ParseObjBase{srcA.getLevel(), srcA.getCommandClass(), srcA.getReadCmdLine(),
+    srcA.getFileLineNumber()}
+{
+    this->m_machineCodeID = srcA.m_machineCodeID;
+    
+    srcA.clearMembers();
+    
+    return;
+}
+
+as::NoOperand & as::NoOperand::operator=(as::NoOperand && rhsA)
+{
+    *(static_cast<ParseObjBase*>(this)) = std::move(static_cast<ParseObjBase&>(rhsA));
+    this->m_machineCodeID = rhsA.m_machineCodeID;
+    
+    rhsA.clearMembers();
+    
+    return *this;
+}
+
+as::NoOperand & as::NoOperand::operator=(const as::NoOperand& rhsA)
+{
+    *(static_cast<ParseObjBase*>(this)) = static_cast<const ParseObjBase&>(rhsA);
+    this->m_machineCodeID = rhsA.m_machineCodeID;
+    
+    return *this;
+}
+
+void as::NoOperand::clearMembers(void)
+{
+    static_cast<ParseObjBase* const>(this)->clearMembers();
+    m_machineCodeID = UINT32_MAX;
+    
+    return;
+}
+
+uint32_t as::NoOperand::getMachineCodeId() const
+{ return m_machineCodeID; }
+
+uint32_t as::NoOperand::setMachineCodeId(const uint32_t machineIdA)
+{
+    auto t_val = m_machineCodeID;
+    m_machineCodeID = machineIdA;
+    
+    return t_val;
+}
+
