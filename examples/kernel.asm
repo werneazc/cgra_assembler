25fd99b4f819a443f60d067819c3c48911670d43
andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
2019-07-21 16:54:08 +0200

    kernel: variable names need to consists of small letters or numbers only
    Signed-off-by: andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>

--- examples/kernel.asm
+++ examples/kernel.asm
@@ -1,25 +1,25 @@
 #Kernel of a 3x3 Convolution
-VAR IAddr 0
-VAR OAddr 2048
+VAR iaddr 0
+VAR oaddr 2048
 VAR place 1
 #Load coefficients to VCGRA data input cache
 LOOP 0 9 1 
-    LOADD IAddr 0 place
-    ADDI IAddr 2
+    LOADD iaddr 0 place
+    ADDI iaddr 2
     ADDI place 2
 POOL
-VAR IAddr 0
+VAR iaddr 0
 VAR place 0
 LOOP 0 9 1 
-    LOADD IAddr 1 place
-    ADDI IAddr 2
+    LOADD iaddr 1 place
+    ADDI iaddr 2
     ADDI place 2
 POOL
 #Load first nine input values to data input cache
 VAR place 1
 LOOP 0 9 1 
-    LOADD IAddr 0 place
-    ADDI IAddr 2
+    LOADD iaddr 0 place
+    ADDI iaddr 2
     ADDI place 2
 POOL
 #Iterate over left input values
@@ -30,24 +30,24 @@ LOOP 9 512 18
     VAR place 1
     SLT_DIC_LINE 1
     LOOP 0 9 1
-        LOADD IAddr 1 place
-        ADDI IAddr 2
+        LOADD iaddr 1 place
+        ADDI iaddr 2
         ADDI place 2
     POOL
     WAIT_READY
-    STORED OAddr 0 0
+    STORED oaddr 0 0
     SLT_DOC_LINE 1
     START
-    ADDI OAddr 2
+    ADDI oaddr 2
     SLT_DIC_LINE 0
     VAR place 1
     LOOP 0 9 1
-        LOADD IAddr 0 place
-        ADDI IAddr 2
+        LOADD iaddr 0 place
+        ADDI iaddr 2
         ADDI place 2
     POOL
     WAIT_READY
-    STORED OAddr 1 0
-    ADDI OAddr 2
+    STORED oaddr 1 0
+    ADDI oaddr 2
 POOL
 FINISH
