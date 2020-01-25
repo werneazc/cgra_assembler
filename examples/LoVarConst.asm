2f8c1c4b45bec1bd133afdccf9ddd430fcbf7d90
andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>
2020-01-25 14:52:29 +0100

    examples: Correct typo in configuration and extend assembler code example
    Signed-off-by: andrewerner <andre.werner-w2m@ruhr-uni-bochum.de>

--- examples/LoVarConst.asm
+++ examples/LoVarConst.asm
@@ -4,5 +4,9 @@ LOOP 0 5 1
     VAR myVar2 myConst
     LOOP 0 myConst myVar
         ADDI myVar2 2
+        SLCT_DIC_LINE 0
+        LOADPC myConst 2
+        LOADCC 80 myVar
     POOL
 POOL
+LOADD myVar 0 181
